893e3807746edcb05a98264238fe3bc56c8d39f3
Monster <monster@MonsterdeMac-mini.local>
2020-10-16 09:27:22 +0800

    1

--- 翻转列表.py
+++ 翻转列表.py
@@ -2,26 +2,26 @@
 
 # 迭代写法
 ## 迭代方法，通过声明一个头指针进行节点与节点之间的链接
-class Solution(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
-def reverseList(self, head):
-    headNode = ListNode(0)
-    while head is not None:
-        temp = head.next
-        head.next = headNode.next
-        headNode.next = head
-        head = temp
-    return headNode.next
+# class Solution1(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+#
+#
+# def reverseList(self, head):
+#     headNode = ListNode(0)
+#     while head is not None:
+#         temp = head.next
+#         head.next = headNode.next
+#         headNode.next = head
+#         head = temp
+#     return headNode.next
 
 
 ## 利用中间变量
 ##时间复杂度O(N)
 ##空间复杂度O(1)
-class Solution(object):
+class Solution2(object):
     def __init__(self, x):
         self.val = x
         self.next = None
@@ -41,7 +41,7 @@ class Solution(object):
 
 ##时间复杂度O(N)
 ##空间复杂度O(1)
-class Solution(object):
+class Solution3(object):
     def __init__(self, x):
         self.val = x
         self.next = None
